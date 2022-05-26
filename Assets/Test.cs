83ab1f280a99400f6df7992b6ab41bf65eff8b21
GreenpixDev <kol-roman.green@mail.ru>
2022-05-26 12:13:10 +0700

    Init commit

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private Vector3 _startPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaY = (float) Math.Sin(Time.time * 2f);
+        transform.position = _startPosition + new Vector3(0, deltaY, 0);
+        transform.Rotate(Time.deltaTime * 4, Time.deltaTime * 16, 0);
+    }
+}
