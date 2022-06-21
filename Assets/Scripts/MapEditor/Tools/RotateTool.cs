3add7ff9d3b31aa63864050873cbca65ee550fad
Aleksey Feofilov <Alexfil888@gmail.com>
2022-06-21 19:14:24 +0700

    add rotation reset

--- Assets/Scripts/MapEditor/Tools/RotateTool.cs
+++ Assets/Scripts/MapEditor/Tools/RotateTool.cs
@@ -2,8 +2,50 @@ using UnityEngine;
 
 namespace MapEditor.Tools
 {
-    public class RotateTool : Tool, ITool
+    public sealed class RotateTool : Tool, ITool
     {
+        private int _clicked;
+        private float _clickTime;
+        private const float ClickDelay = 0.5f;
+
+        protected override void OnMouseDown()
+        {
+            base.OnMouseDown();
+
+            if (Time.time - _clickTime > ClickDelay)
+            {
+                _clicked = 0;
+            }
+
+            _clicked++;
+            _clickTime = Time.time;
+
+            if (_clicked == 2)
+            {
+                OnDoubleClick();
+            }
+        }
+
+        private void OnDoubleClick()
+        {
+            var rotation = transform.parent.parent.rotation.eulerAngles;
+
+            switch (tag)
+            {
+                case "OX":
+                    MainTools.SetRotation(Quaternion.Euler(0, rotation.y, rotation.z));
+                    break;
+
+                case "OY":
+                    MainTools.SetRotation(Quaternion.Euler(rotation.x, 0, rotation.z));
+                    break;
+
+                case "OZ":
+                    MainTools.SetRotation(Quaternion.Euler(rotation.x, rotation.y,0));
+                    break;
+            }
+        }
+
         protected override void Change() => ((ITool)this).Change(Speed * rotateSpeed, tag);
 
         void ITool.ChangeOx(float speed)
