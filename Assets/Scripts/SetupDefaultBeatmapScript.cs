f4823866a20e9106bea70f73d9f5a2858789ab09
GreenpixDev <kol-roman.green@mail.ru>
2022-06-25 03:05:53 +0700

    Добавил разные функции плавности

---
+++ Assets/Scripts/SetupDefaultBeatmapScript.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.IO.Compression;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SetupDefaultBeatmapScript : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            string folder = $"{Application.persistentDataPath}/Songs";
+
+            if (Directory.Exists(folder)) return;
+            
+            var songs = Resources.Load<TextAsset>("Songs");
+            string zip = $"{folder}.zip";
+            File.WriteAllBytes(zip, songs.bytes);
+                
+            ZipFile.ExtractToDirectory(zip, Application.persistentDataPath);
+        }
+    }
+}
